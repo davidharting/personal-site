0a5378304a60a9e19165b0e2c50874b283cc04e7
David Harting <davideharting@gmail.com>
2019-01-19 15:33:28 -0500

    kind of improve image quality of profile pic

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -48,9 +48,9 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/david.jpg/" }) {
       childImageSharp {
-        fixed(width: 50, height: 50) {
+        fixed(width: 50, height: 50, quality: 100) {
           ...GatsbyImageSharpFixed
         }
       }
